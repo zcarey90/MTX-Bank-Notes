31ce6e9ed449f224f9e1cd2bdc5c50ddabb4a85b
Zacharey Carey <zac4ncstate@aol.com>
2020-12-17 16:11:34 -0500

    mvp

--- app.js
+++ app.js
@@ -2,32 +2,36 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const multer = require("multer");
-const { TesseractWorker } = require("tesseract.js");
-const worker = new TesseractWorker();
+const createWorker = require("tesseract.js");
+// const { TesseractWorker } = require("tesseract.js");
+// const worker = new TesseractWorker();
 // Storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./uploads");
-  }
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage: storage }).single("avatar");
+
 app.set("view engine", "ejs");
 
 // Routes
 
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render("index");
 });
 
 app.post("/upload", (req, res) => {
-  upload(req, res, err => {
-      console.log(req.file);
+  upload(req, res, (err) => {
+    console.log(req.file);
   });
 });
-
 
 // Start up our server
 
 const PORT = 5000 || process.env.PORT;
-app.listen(PORT, () => console.log('Hey I am running on port ${PORT}'));
+app.listen(PORT, () => console.log("Hey I am running on port ${PORT}"));
