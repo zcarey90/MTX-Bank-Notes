15c5e8791d8b9150b1609f50d1a2d0f22dd668b9
Zacharey Carey <zac4ncstate@aol.com>
2020-12-30 13:40:19 -0500

    added css

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).single("avatar");
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 // Routes
 
